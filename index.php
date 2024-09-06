ae1a6dacc86bd6afddb4a9720527cb1f39cc6304
JOKErzkie <youngzues48@gmail.com>
2024-09-06 15:11:26 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "I am YangZkie <br>";
+    echo "I leave in panaon <br>";
+    echo "and i am pogi😎";
+
+    ?>
+    <h1>Eyyy..</h1>
+</body>
+</html>
